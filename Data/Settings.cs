b930b061f3a40ce4d21c32d365dbf3ea6976fe82
Enn3Developer <arturpeshko39@gmail.com>
2025-02-24 17:54:21 +0100

    cleaned code

--- Data/Settings.cs
+++ Data/Settings.cs
@@ -3,7 +3,6 @@ using System.ComponentModel;
 using System.Text.Json.Serialization;
 using Newtonsoft.Json;
 using ProjBobcat.Class.Helper;
-using ProjBobcat.Class.Model;
 
 namespace NewRGB.Data;
 
@@ -11,8 +10,8 @@ public class Settings
 {
     [System.Text.Json.Serialization.JsonIgnore]
     public readonly uint MaxMemoryValue =
-        (uint)(RoundToTwoPower((SystemInfoHelper.GetMemoryUsage() ?? new MemoryInfo(8192.0 * 2.0, 0.0, 0.0, 0.0))
-            .Total) / 2.0);
+        (uint)RoundToTwoPower(SystemInfoHelper.GetMemoryUsage()?
+            .Total / 2.0 ?? 8192.0);
 
     [System.Text.Json.Serialization.JsonIgnore]
     public const uint MinMemoryValue = 4096;
